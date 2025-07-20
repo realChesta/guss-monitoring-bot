88c5ed46e0ce31e4b51da9aca29b5368c239e151
Kyrill <kyrill@nunu.ai>
2025-07-20 20:26:26 +0200

    add support for custom data dirs, add nixpacks

--- gussbot/storage.py
+++ gussbot/storage.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import BaseModel
 
 from gussbot.types import Apartment, ApartmentFilter
@@ -8,16 +9,20 @@ class Settings(BaseModel):
     apartments: list[Apartment]
     apartment_filter: ApartmentFilter
 
+    @classmethod
+    def get_file_path(cls) -> str:
+        return os.path.join(os.environ.get("SETTINGS_DIR", "settings"), "settings.json")
+
     
     @classmethod
     def load(cls) -> "Settings":
         try:
-            return Settings.model_validate_json(open("settings.json").read())
+            return Settings.model_validate_json(open(cls.get_file_path()).read())
         except FileNotFoundError:
             return Settings(apartments=[], apartment_filter=ApartmentFilter(colony=[], apartment_rooms=[], min_apartment_area=0, max_apartment_area=0, min_price=0, max_price=0))
     
     def save(self) -> None:
-        open("settings.json", "w").write(self.model_dump_json())
+        open(self.get_file_path(), "w").write(self.model_dump_json())
         
         
       
